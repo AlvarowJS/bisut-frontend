defa7de8172d5e844589c6a1fbb3e999f803185c
AlvarowJS <alvaruwiiu@hotmail.com>
2024-09-18 01:06:17 -0500

    filtros y tabla para kardex

--- src/views/almacen/kardex/Kardex.jsx
+++ src/views/almacen/kardex/Kardex.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import KardexTable from './KardexTable'
-import { Col, Input, Label, Row } from 'reactstrap'
+import { Button, Col, Input, Label, Row } from 'reactstrap'
 import KardexForm from './KardexForm'
 import bdAdmin from '../../../api/bdAdmin'
+import Select from "react-select";
 const URL = "v1/kardex"
 const URLPRODUCTO = "v1/productos"
 const URLTIENDA = "v1/almacen"
 
 const Kardex = () => {
     const [search, setSearch] = useState()
-    
+
+    // autorizacion
+    const token = localStorage.getItem("accessToken");
+    const getAuthHeaders = () => ({
+        headers: {
+            Authorization: "Bearer " + token,
+        },
+    });
 
     // Filters
     const [fechaInicio, setFechaInicio] = useState()
@@ -22,6 +30,10 @@ const Kardex = () => {
     const [productos, setProductos] = useState()
     const [tiendas, setTiendas] = useState()
 
+    // Seleccionables
+    const [producto, setProducto] = useState()
+    const [tienda, setTienda] = useState()
+
     const handleFilter = (e) => {
         setSearch(e.target.value);
     }
@@ -31,20 +43,51 @@ const Kardex = () => {
     //         .then(res => res.data)
     //         .catch(err => console.log(err))
     // }, [])
+
+    const handleChallengeProducto = (selected) => {
+        setProducto(selected)
+        console.log(selected)
+    }
+
+    const handleChallengeTienda = (selected) => {
+        setTienda(selected)
+    }
+
+    const handleChangeFechaInicio = (event) => {
+        setFechaInicio(event.target.value)
+    }
+
+    const handleChangeFechaFin = (event) => {
+        setFechaFin(event.target.value)
+    }
     useEffect(() => {
-        bdAdmin.get(URLPRODUCTO)
+        bdAdmin.get(URLPRODUCTO, getAuthHeaders())
             .then(res => setProductos(res.data))
             .catch(err => console.log(err))
 
-        bdAdmin.get(URLTIENDA)
+        bdAdmin.get(URLTIENDA, getAuthHeaders())
             .then(res => setTiendas(res.data))
             .catch(err => console.log(err))
     }, [])
-    
+
+    const optionsProducto = productos?.map((option) => ({
+        value: option?.id,
+        label: option?.item + ' ' + option?.descripcion
+    }))
+
+    const optionsTienda = tiendas?.map((option) => ({
+        value: option?.id,
+        label: option?.nombre
+    }))
+
+
+
 
     const buscarKardex = () => {
-        bdAdmin.get(`${URL}?producto${productoId}&fechaInicio=${fechaInicio}&fechaFin=${fechaFin}&tienda${tiendaId}`)
-            .then(res => res.data)
+        bdAdmin.get(`${URL}?producto=${producto?.value ?? ''}&fechaInicio=${fechaInicio ?? ''}&fechaFin=${fechaFin ?? ''}&tienda=${tienda?.value ?? ''}`, getAuthHeaders())
+            .then(res => {
+                setData(res.data)
+            })
             .catch(err => console.log(err))
     }
     return (
@@ -52,18 +95,54 @@ const Kardex = () => {
             <h3>Kardex</h3>
             <Row>
                 <Col sm="3">
-                    Fecha Inicio 
+                    <Label className="me-1" for="search-input">
+                        Tienda
+                    </Label>
+                    <Select
+                        id="tienda"
+                        value={tienda}
+                        onChange={handleChallengeTienda}
+                        options={optionsTienda}
+                        isSearchable={true}
+                        placeholder="No especifica"
+                    />
                 </Col>
                 <Col sm="3">
-                    Fecha Fin
+                    <Label className="me-1" for="search-input">
+                        Producto
+                    </Label>
+                    <Select
+                        id="producto"
+                        value={producto}
+                        onChange={handleChallengeProducto}
+                        options={optionsProducto}
+                        isSearchable={true}
+                        placeholder="No especifica"
+                    />
                 </Col>
-                <Col sm="3">
-                    Producto
+                <Col sm="2">
+                    <Label>
+                        Fecha Inicio
+                    </Label>
+                    <Input 
+                        type='date'
+                        onChange={handleChangeFechaInicio}
+                    />
                 </Col>
-                <Col sm="3">
-                    Tienda
+                <Col sm="2">
+                <Label>
+                        Fecha Inicio
+                    </Label>
+                    <Input 
+                        type='date'
+                        onChange={handleChangeFechaFin}
+                    />
                 </Col>
-
+                <Col sm="2">
+                    <Button style={{marginTop: 22}} color="warning" onClick={buscarKardex}>
+                        Filtrar
+                        </Button>
+                </Col>
             </Row>
             <Row className="mb-2">
                 <Col sm="6">
--- src/views/almacen/kardex/KardexTable.jsx
+++ src/views/almacen/kardex/KardexTable.jsx
@@ -1,8 +1,118 @@
 import React from 'react'
+import { Badge, Card } from 'reactstrap'
+import DataTable from 'react-data-table-component'
+import './KardexTable.css' // Importa los estilos personalizados
 
-const KardexTable = () => {
+const KardexTable = ({ data }) => {
+  const columns = [
+    {
+        sortable: true,
+        name: 'ID',
+        minWidth: '50px',
+        maxWidth: '100px',
+        selector: row => row?.id
+    },
+    {
+      sortable: true,
+      name: 'Fecha',
+      minWidth: '100px',
+      maxWidth: '150px',
+      selector: row => row?.fecha
+    },
+    {
+      sortable: true,
+      name: 'Documento',
+      minWidth: '150px',
+      maxWidth: '200px',
+      selector: row => row?.documento
+    },
+    {
+      sortable: true,
+      name: 'Operación',
+      minWidth: '150px',
+      maxWidth: '200px',
+      selector: row => row?.operacion.nombre
+    },
+    {
+      sortable: true,
+      name: 'Cantidad Entrada',
+      minWidth: '100px',
+      maxWidth: '150px',
+      selector: row => row?.cantidadEntrada
+    },
+    {
+      sortable: true,
+      name: 'Valor Unitario Entrada',
+      minWidth: '100px',
+      maxWidth: '150px',
+      selector: row => row?.vuEntrada
+    },
+    {
+      sortable: true,
+      name: 'Valor Total de entrada',
+      minWidth: '100px',
+      maxWidth: '150px',
+      selector: row => row?.vtEntrada
+    },
+    {
+      sortable: true,
+      name: 'Cantidad de Salida',
+      minWidth: '100px',
+      maxWidth: '150px',
+      selector: row => row?.cantidadSalida
+    },
+    {
+      sortable: true,
+      name: 'Valor Unitario de Salida',
+      minWidth: '100px',
+      maxWidth: '150px',
+      wrap: true,
+      selector: row => row?.vuSalida
+    },
+    {
+      sortable: true,
+      name: 'Valor total de salida',
+      minWidth: '100px',
+      maxWidth: '150px',
+      selector: row => row?.vtSalida
+    },
+    {
+      sortable: true,
+      name: 'Cantidad de saldo',
+      minWidth: '100px',
+      maxWidth: '150px',
+      wrap: true,
+      selector: row => row?.cantidadSaldo
+      
+    },
+    {
+      sortable: true,
+      name: 'Valor Unitario Saldo',
+      minWidth: '100px',
+      maxWidth: '150px',
+      selector: row => row?.vuSaldo
+    },
+    {
+      sortable: true,
+      name: 'Valor Total Saldo',
+      minWidth: '100px',
+      maxWidth: '150px',
+      selector: row => row?.vtSaldo
+    }
+  ]
+
   return (
-    <div>KardexTable</div>
+    <Card className='mt-2'>
+      <DataTable
+        noHeader
+        pagination
+        className='react-dataTable'
+        columns={columns}
+        subHeaderWrap	
+        subHeader
+        data={data}
+      />
+    </Card>
   )
 }
 
