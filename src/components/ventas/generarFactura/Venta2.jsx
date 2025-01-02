507a786f8704c36e1e0d0db2b5ba23b9488464db
Alvaro Japa Salazar <alvarojapasalazar@MacBook-Air-de-Alvaro.local>
2025-01-02 13:43:51 -0500

    corrigiendo sumas y quitando email innecesarios

--- src/components/ventas/generarFactura/Venta2.jsx
+++ src/components/ventas/generarFactura/Venta2.jsx
@@ -7,22 +7,17 @@ const Venta2 = ({
 }) => {
     return (
         <Row className="border rounded p-1">
-            <Col className="d-flex">
 
-                <label htmlFor="">%Desc. </label>
-                <input type="text" className="form-control form-control-sm" {...register('descuento')}/>
-
-            </Col>
-            <Col className="d-flex">
+            <Col className="d-flex" sm="4">
                 <label htmlFor="">Puntos </label>
-                <input type="text" className="form-control form-control-sm" {...register('puntos')}/>
+                <input type="text" className="form-control form-control-sm" aria-label="Small" {...register('puntos')} />
 
             </Col>
-            <Col>
-                <input type="checkbox" className="form-check-input" {...register('regalo')}/>
+            <Col sm="2">
+                <input type="checkbox" className="form-check-input" {...register('regalo')} />
                 Regalo
             </Col>
-            <Col className="d-flex gap-2">
+            <Col className="d-flex gap-2" sm="3">
                 <div className="form-check">
                     <input
                         className="form-check-input"
@@ -30,7 +25,7 @@ const Venta2 = ({
                         name="tipofactura"
                         id="remision"
                         value="remision"
-                        {...register('tipo_factura')} 
+                        {...register('tipo_factura')}
                     />
                     <label className="form-check-label" htmlFor="remision">
                         Remisión
@@ -43,26 +38,24 @@ const Venta2 = ({
                         name="tipofactura"
                         id="factura"
                         value="factura"
-                        {...register('tipo_factura')} 
+                        {...register('tipo_factura')}
                     />
                     <label className="form-check-label" htmlFor="factura">
                         Factura
                     </label>
                 </div>
             </Col>
-            <Col>
-                <input type="checkbox" className="form-check-input" {...register('venta_credito')}/>
-                Venta Credito
+            <Col sm="3">
+                <label>Atendido por: </label>
+                <Select
+                    id="user"
+                    value={user}
+                    onChange={handleUserChange}
+                    options={userOptions}
+                    isSearchable={true}
+                    placeholder="No especifica"
+                />
             </Col>
-            <label>Atendido por: </label>
-            <Select
-                id="user"
-                value={user}
-                onChange={handleUserChange}
-                options={userOptions}
-                isSearchable={true}
-                placeholder="No especifica"
-            />
         </Row>
     )
 }
